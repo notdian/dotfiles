6b4c9606ec5db0e8a803af91d014eb50a3b4aea8
Dian Fishekqi <dian@fishekqi.com>
2023-12-11 18:26:17 +0100

    remove some legacy stuff

--- openbox/autostart.sh
+++
@@ -1,9 +0,0 @@
-numlockx &
-nitrogen --set-zoom-fill --restore &
-( sleep 2 && tint2 & )
-( sleep 2 && nm-applet & )
-( sleep 4 && compton & )
-( sleep 15 && timeout 1m elinks --auto-submit 'http://172.16.0.12/CP/sq.html' &)
-( sleep 6 && bingwallpaper & )
-( sleep 10 && synapse -s &) 
-( sleep 4 && syndaemon -i .5 -t -d &)
